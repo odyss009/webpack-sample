e2910f3f2195d1e635b49d7f90956cf6ba5a4215
odyss009 <odyss009@gmail.com>
2016-07-27 15:49:03 +0900

    remove swf option

--- webpack.config.js
+++ webpack.config.js
@@ -48,10 +48,6 @@ module.exports = {
             //   test: /\.(png|woff|woff2|eot|ttf|svg)(\?.*)?$/,
             //   loader: 'url-loader?limit=100000'
             // },
-            // {
-            //     test: /\.swf$/,
-            //     loader: "file?name=./[name].swf"
-            // },
             {
                 test: /global\/*/,
                 loader: "exports?window.lc"
